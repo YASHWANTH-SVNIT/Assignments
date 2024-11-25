a421121283f6f50ae5c0b580e3adeda29e5c48b3
Yashwanth <u24ai118@aid.svnit.ac.in>
2024-11-25 20:54:02 +0530

    A

--- Assignment107.c
+++ Assignment107.c
@@ -1,24 +1,40 @@
 // IMPLEMENT A FUNCTION NAMED AS FLIP; WHICH WILL TAKE A NUMBER AS INPUT AND FLIP ITS LAST N DIGITS 
 
 #include<stdio.h>
+#include<math.h>
 
 int flip(int,int);
 
 int main()
 {
-    int num,n;
+    int num,n,digits=0;
     printf("Enter the number: ");
     scanf("%d",&num);
     printf("Enter the number of digits to flipped from last: ");
     scanf("%d",&n);
-    printf("\nThe reverse of the nuber is %d",flip(num,n));
+
+    int temp=num;
+    while(temp!=0)
+    {
+        temp/=10;
+        digits++;
+    }
+
+    printf("\nThe reverse of the number is %d",flip(num,n));
     return 0;
 }
 
 int flip(int num,int n)
 {
-    for(int i=0;i<n;i++)
+    int temp=num;
+    num=num/pow(10,n);
+
+    for(int i=1;i<=n;i++)
     {
-        
+        int rem=temp%10;
+        num=num*10+rem;
+        temp/=10;
     }
+    
+    return num;
 }
--- Assignment108D.c
+++ Assignment108D.c
@@ -1,1 +1,32 @@
 //D. REVERSE A GIVEN STRING 
+
+#include<stdio.h>
+
+char *rev_string(char *,int);
+
+int main()
+{
+    char str[100];
+    int size=0;
+    printf("Enter the string: ");
+    scanf(" %[^\n]s",str);
+    
+    for(int i=0;str[i]!='\0';i++)
+        size++;
+
+    printf("\nOriginal string: %s",str);
+    printf("\nReverse of the string: %s",rev_string(str,size));
+    return 0;
+}
+
+char *rev_string(char *ptr,int n)
+{
+    char temp;
+    for(int i=0;i<n/2;i++)
+    {
+        temp=*(ptr+i);
+        *(ptr+i)=*(ptr+n-1-i);
+        *(ptr+n-1-i)=temp;
+    }
+    return ptr;
+}
